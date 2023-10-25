0467f3043fb1558de6856364b83097833da1d62e
Iuri <nicoguloso2@gmail.com>
2023-10-25 18:10:22 +0200

    frontend fixes

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,33 +1,33 @@
 // Utiliser l'API pour vérifier l'authentification de l'utilisateur //
-async function seConnecter(username, password) {
+async function seConnecter(email, password) {
     const response = await fetch('http://localhost:5678/api/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ email, password })
     });
     
     if (response.ok) {
-        const data = await response.json();
-        return data.success;
+        const login = await response.json();
+        return login.success;
     } else {
         return false;
     }
 }
 
-document.getElementById('login').addEventListener('submit', async (e) => {
-    console.log("Le formulaire a été soumis");
-    e.preventDefault();
-    const username = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+document.getElementById('login').addEventListener('submit', async (event) => {
+    console.log("test");
+    event.preventDefault();
+    const email = document.querySelector('input[type="email"]').value;
+    const password = document.querySelector('input[type="password"]').value;
   
     // Appel à la fonction d'authentification
-    const loginSuccessful = await seConnecter(username, password);
+    const loginSuccessful = await seConnecter(email, password);
   
     if (loginSuccessful) {
       window.location.href = 'index.html';
     } else {
-      document.getElementById('errorMessage').innerText = 'Erreur dans l\'email ou le mot de passe';
+      document.getElementById('login-error').style.display = 'flex';
     }
   });
